7d68e2b9ebcdbcf560ff57e892b4adf3c7aa4450
Louis-Etienne <louis-etienne.messier.1@ulaval.ca>
2024-07-08 09:14:32 -0400

    ajout de la fonctionalité de jump

--- filter.py
+++ filter.py
@@ -31,7 +31,6 @@ def save_notes(notes):
             json.dump(notes, f, indent=4)
 
 def open_image(list_path, list_files):
-    pc.copy(list_files[index])
     subprocess.run(["tev", ":composite", list_path[index]])
 
 def open_file(list_path, list_files):
@@ -39,7 +38,41 @@ def open_file(list_path, list_files):
     thread = threading.Thread(target=open_image, args=(list_path, list_files))
     thread.start()
 
-# Set up environement values
+def jump(entry, list_path, list_files, notes, label_index, label_name, text, reasons_var, reasons):
+    val = entry.get()
+    entry.delete(0, tk.END)
+    if val.isdigit():
+        num = int(val)
+        if num >= 0 and num < len(list_files):
+            global index
+            index = num -1
+            next(list_path, list_files, notes, label_index, label_name, text, reasons_var, reasons)
+
+def next(list_path, list_files, notes, label_index, label_name, text, reasons_var, reasons):
+    global index
+    if index < len(list_files) -1:
+        index = index + 1
+        label_index.config(text=f"Index : {index}/{len(list_files)}")
+        label_name.config(text=f"name : {list_files[index]}")
+        text.delete("1.0", tk.END)
+        text.insert(tk.END, notes[list_files[index]]["note"])
+        for i,reason in enumerate(reasons):
+            reasons_var[i].set(notes[list_files[index]][reason])
+        open_file(list_path, list_files)
+
+def back(list_path, list_files, notes, label_index, label_name, text, reasons_var, reasons):
+    global index
+    if index > 0:
+        index = index - 1
+        label_index.config(text=f"Index : {index}/{len(list_files)}")
+        label_name.config(text=f"name : {list_files[index]}")
+        text.delete("1.0", tk.END)
+        text.insert(tk.END, notes[list_files[index]]["note"])
+        for i,reason in enumerate(reasons):
+            reasons_var[i].set(notes[list_files[index]][reason])
+        open_file(list_path, list_files)
+
+# Environement setup
 reasons = ['Misplaced', 'Too big', 'Too small', 'Weird shadow', 'Too bright', 'Noisy envamp', 'Looks off', 'Other']
 list_path = glob.glob("D:/@Université/STAGES/STAGE1/globus_sharing/GT_emission_envmap/*/*.exr")
 list_files = [isolate_exr(f) for f in list_path]
@@ -53,6 +86,10 @@ root.title('Filtering app')
 
 button_frame = tk.Frame(root)
 label_index = tk.Label(root, text= f"Index : {index}/{len(list_files)}")
+entry_frame = tk.Frame(root)
+entry = tk.Entry(entry_frame, width=10)
+entry_label = tk.Label(entry_frame, text="Enter number to jump to index : ")
+entry_jump = tk.Button(entry_frame, text="Jump", command=lambda:jump(entry, list_path, list_files, notes, label_index, label_name, text, reasons_var, reasons))
 label_name = tk.Label(root, text=f"name : {list_files[index]}")
 text = tk.Text(root, height=5, width=50)
 
@@ -67,14 +104,8 @@ reasons_buttons = [tk.Checkbutton(reason_frame, text=reasons[i], variable=reason
 for button in reasons_buttons:
     button.pack()
 
-
-
-
-
-
-
-back_button = tk.Button(button_frame, text="Back", command=lambda: back(list_path, list_files, notes, label_index, label_name, text, reasons_var))
-next_button = tk.Button(button_frame, text="Next", command=lambda: next(list_path, list_files, notes, label_index, label_name, text, reasons_var))
+back_button = tk.Button(button_frame, text="Back", command=lambda: back(list_path, list_files, notes, label_index, label_name, text, reasons_var, reasons))
+next_button = tk.Button(button_frame, text="Next", command=lambda: next(list_path, list_files, notes, label_index, label_name, text, reasons_var, reasons))
 
 def on_text_change(event):
     notes[list_files[index]]["note"] = text.get("1.0", tk.END).strip()
@@ -83,6 +114,11 @@ def on_text_change(event):
 text.bind('<<Modified>>', on_text_change)
 
 label_index.pack()
+entry_frame.pack()
+entry_jump.pack(side=tk.RIGHT)
+entry.pack(side=tk.RIGHT)
+entry_label.pack(side=tk.LEFT)
+
 label_name.pack()
 text.pack()
 reason_frame.pack()
@@ -92,7 +128,7 @@ next_button.pack(side=tk.RIGHT)
 
 # load the first image
 index=-1
-next(list_path, list_files, notes, label_index, label_name, text, reasons_var)
+next(list_path, list_files, notes, label_index, label_name, text, reasons_var, reasons)
 
 # starts the window loop
 root.mainloop()
